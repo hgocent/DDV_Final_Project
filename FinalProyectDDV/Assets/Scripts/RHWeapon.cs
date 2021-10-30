6fc6e29ca522db85b903bcda982e97e67424b0b9
DeMaT87 <92327438+DeMaT87@users.noreply.github.com>
2021-10-30 18:35:30 -0300

    Movimiento Enemigos
    Se agrego movimiento por waypoints a 3 enemigos de prueba

--- FinalProyectDDV/Assets/Scripts/RHWeapon.cs
+++ FinalProyectDDV/Assets/Scripts/RHWeapon.cs
@@ -18,7 +18,8 @@ public class RHWeapon : MonoBehaviour
         
     }
     public void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player"){
+        if (other.tag == "Player")
+        {
             Instantiate(RWeapon, RHand);
             Destroy(gameObject);
         }
