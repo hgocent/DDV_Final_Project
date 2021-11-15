3e2a4ec89ec05081b131d31f4c08fb964e85dcef
DeMaT87 <92327438+DeMaT87@users.noreply.github.com>
2021-11-14 21:11:07 -0300

    update
    minor update

--- FinalProyectDDV/Assets/Scripts/CameraController.cs
+++ FinalProyectDDV/Assets/Scripts/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {   
     public GameObject[] cameras;
+    
         // Start is called before the first frame update
     void Start()
     {
