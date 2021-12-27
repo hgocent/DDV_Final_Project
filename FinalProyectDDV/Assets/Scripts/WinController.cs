a4863dd675888282f07afecfa1d036929e853611
DeMaT87 <92327438+DeMaT87@users.noreply.github.com>
2021-12-27 17:58:37 -0300

    update
    se modificaron las puertas

--- FinalProyectDDV/Assets/Scripts/WinController.cs
+++ FinalProyectDDV/Assets/Scripts/WinController.cs
@@ -43,14 +43,14 @@ public class WinController : MonoBehaviour
     //move towards a target at a set speed.
     public void OpenDoors() 
     {
-        float speed = 1f;
+        //float speed = 1f;
         
         if (exitDoorOpened == false)
         { 
-            Vector3 directionOfTravel01 = new Vector3(260f, 0, 0); //amount of space to move relatively to speed of 1f
-            Vector3 directionOfTravel02 = new Vector3(-260f, 0, 0);
+            Vector3 directionOfTravel01 = new Vector3(33f, 18.58f, 393.91f); //amount of space to move relatively to speed of 1f
+            Vector3 directionOfTravel02 = new Vector3(-37f, 18.58f, 396.89f);
             
-            GameObject.Find("Door_Left_01").transform.Translate
+            /*GameObject.Find("Door_Left_01").transform.Translate
             ((directionOfTravel01.x * speed * Time.deltaTime),
             (directionOfTravel01.y * speed * Time.deltaTime),
             (directionOfTravel01.z * speed * Time.deltaTime),
@@ -60,7 +60,9 @@ public class WinController : MonoBehaviour
             ((directionOfTravel02.x * speed * Time.deltaTime),
             (directionOfTravel02.y * speed * Time.deltaTime),
             (directionOfTravel02.z * speed * Time.deltaTime),
-            Space.World);    
+            Space.World);*/
+            GameObject.Find("Door_Left_01").transform.position = directionOfTravel01;
+            GameObject.Find("Door_Left_02").transform.position = directionOfTravel02;    
                
             
             exitDoorOpened = true;
